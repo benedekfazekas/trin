378ab8826e9a7d785cdfa9c92aecfe65a8ddabfe
Benedek Fazekas <benedek.fazekas@gmail.com>
2019-04-17 10:11:14 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# trin
+
+A Clojure library designed to ... well, that part is up to you.
+
+## Usage
+
+FIXME
+
+## License
+
+Copyright © 2019 Benedek Fazekas
---
+++ test/thomasa/trin_test.clj
@@ -0,0 +1,7 @@
+(ns thomasa.trin-test
+  (:require [clojure.test :as t]
+            [thomasa.trin :as trin]))
+
+(t/deftest a-test
+  (t/testing "FIXME, I fail."
+    (t/is (= 1 1))))
