77a1fce517fd477226c1baaa8c1bca0a0d7e368a
Benedek Fazekas <benedek.fazekas@gmail.com>
2019-04-25 16:09:59 +0100

    Use rewrite-clj nodes as AST nodes
    basically enrich them with AST like info. would be nice to follow
    tools.analyzer AST spec

--- trin/src/thomasa/trin.clj
+++ trin/src/thomasa/trin.clj
@@ -1,8 +1,22 @@
 (ns thomasa.trin
-  (:require [rewrite-clj.zip :as zip]))
+  (:require [rewrite-clj.zip :as zip]
+            [rewrite-clj.zip.subedit :as zsub]
+            [clojure.zip :as clj-zip]))
 
 (defn foo
   "I don't do a whole lot."
   []
   (println "Hello, World!")
   :foo)
+
+(defn all-zlocs
+  "Generate a seq of all zlocs in a depth-first manner"
+  [zipper]
+  (take-while (complement zip/end?) (iterate zip/next zipper)))
+
+(defn attach-ast-info [loc ast-key ast-value-fn]
+  (clj-zip/edit loc (fn [m] (update-in m [:ast-info ast-key] ast-value-fn))))
+
+
+
+(comment (zsub/subzip ))
--- trin/test/thomasa/trin_test.clj
+++ trin/test/thomasa/trin_test.clj
@@ -1,7 +1,27 @@
 (ns thomasa.trin-test
   (:require [clojure.test :as t]
+            [rewrite-clj.zip :as zip]
             [thomasa.trin :as trin]))
 
 (t/deftest a-test
   (t/testing "FIXME, I don't test much"
     (t/is (= 1 1))))
+
+(t/deftest use-rewrite-clj-node-as-AST
+  (t/is (= "foo" (-> (zip/of-string "[1 2 3]")
+                     (trin/attach-ast-info :foo (constantly "foo"))
+                     first
+                     :ast-info
+                     :foo))
+        "Failed to attach data to rewrite-clj node")
+  (t/is (= [2 "foo"] (-> (zip/of-string "[1 2 3]")
+                         zip/down
+                         zip/right
+                         (trin/attach-ast-info :foo (constantly "foo"))
+                         zip/up
+                         zip/root
+                         zip/edn
+                         trin/all-zlocs
+                         (nth 2)
+                         ((juxt zip/sexpr (comp :foo :ast-info first)))))
+        "Failed to make attached data persistent across zipper movements"))
