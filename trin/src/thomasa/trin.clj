67e703241f594be0050bd2627a6a656e54ff048d
Benedek Fazekas <benedek.fazekas@gmail.com>
2019-04-28 21:54:51 +0100

    Handle let in let, eq shadowing
    Always recur to `parse-loc` when subtree can be a complex sexpr, some
    kind of body

--- trin/src/thomasa/trin.clj
+++ trin/src/thomasa/trin.clj
@@ -20,49 +20,75 @@
 (defn- add-to-locals [binding-node init-node locals]
   (swap! locals assoc (zip/sexpr binding-node) (zip/sexpr init-node)))
 
-(declare parse-let-loc)
+(declare parse-loc)
+
+(defn- parse-bindings* [locals env node]
+  (let [binding node
+        init (->> (merge env {:locals @locals})
+                  (partial parse-loc)
+                  (zsub/subedit-node (zip/right binding)))]
+    (add-to-locals binding init locals)
+    (if-let [next-binding (zip/right init)]
+      (parse-bindings* locals env next-binding)
+      init)))
 
 (defn- parse-bindings [locals env node]
-  (if-let [binding (and (not (zip/end? (zip/next node)))
-                        (zip/next node))]
-    (let [init (->> (merge env {:locals @locals})
-                    (partial parse-let-loc)
-                    (zsub/subedit-node (zip/next binding)))]
-      (add-to-locals binding init locals)
-      (parse-bindings locals env init))
+  (if-let [first-binding (zip/down node)]
+    (zip/up (parse-bindings* locals env first-binding))
+    node))
+
+(defn- parse-let-body* [env node]
+  (let [node (zsub/subedit-node node (partial parse-loc env))]
+    (if-let [next-body-loc (zip/right node)]
+      (parse-let-body* env next-body-loc)
+      node)))
+
+(defn- parse-let-body [locals env node]
+  (if-let [first-body-loc (zip/right node)]
+    (parse-let-body* (merge env {:locals @locals}) first-body-loc)
     node))
 
 (defn parse-let-loc [env loc-let]
   (let [locals (atom (:locals env))]
-      (zip/prewalk
-       loc-let
-       (fn [node]
-         (println (zip/sexpr node))
-         (let [locs @locals]
-             (cond-> node
+    (-> (zip/down loc-let)
+        zip/right
+        (zsub/subedit-node (partial parse-bindings locals env))
+        ((partial parse-let-body locals env))
+        zip/up)))
 
-               :always
-               (attach-ast-info :env (fn [env] (assoc env :locals locs)))
+(defn- skip-to-bottom [loc]
+  (or (some-> (zip/down loc)
+              zip/rightmost
+              skip-to-bottom)
+      (zip/rightmost loc)))
 
-               ((set (keys locs)) (zip/sexpr node))
-               (-> (attach-ast-info :op (constantly :local))
-                   (attach-ast-info :init (constantly (locs (zip/sexpr node)))))
+(defn- let-loc?
+  [node]
+  (and (zip/seq? node)
+       (= 'let (zip/sexpr (zip/down node)))))
 
-               (and (= :vector (zip/tag node))
-                    (= 'let (zip/sexpr (zip/left node))))
-               (-> (zsub/subedit-node (partial parse-bindings locals env))
-                   zip/down
-                   zip/rightmost)
+(defn- locals-contains?
+  [locs node]
+  ((set (keys locs)) (zip/sexpr node)))
 
-               ))))))
+(defn parse-node [env node]
+  (let [locs (:locals env)]
+    (cond-> node
+
+      (locals-contains? locs node)
+      (-> (attach-ast-info :op (constantly :local))
+          (attach-ast-info :init (constantly (locs (zip/sexpr node)))))
+
+      :always
+      (attach-ast-info :env (fn [env] (assoc env :locals locs))) ;; or update with merge?!
+
+      (let-loc? node)
+      (-> (zsub/subedit-node (partial parse-let-loc env))
+          skip-to-bottom))))
 
 (defn parse-loc [env loc]
   (zip/prewalk
    loc
-   (fn [node] (and (zip/seq? node)
-                   (= 'let (zip/sexpr (zip/down node)))))
-   (fn [node]
-     (zsub/subedit-node node (partial parse-let-loc env)))))
+   (partial parse-node env)))
 
-;; - improve binding parsing
-;;   - should be aware of already available locals (shadowing)
+;; -- empty things: empty let bindings, empty let body
--- trin/test/thomasa/trin_test.clj
+++ trin/test/thomasa/trin_test.clj
@@ -38,28 +38,54 @@
     [b c])
   (println \"baz\"))")
 
-(t/deftest parse-let-loc-test
-  (let [embedded-let-with-locals (trin/parse-loc {} (zip/of-string embedded-let))
-        locs (trin/all-zlocs embedded-let-with-locals)]
-    (t/is (= '#{a b c} (-> embedded-let-with-locals
-                           zip/down
-                           zip/right
-                           zip/right
-                           zip/down
-                           zip/right
-                           zip/right
-                           ((fn [node] (println (zip/sexpr node)
-                                                "--"
-                                                (-> node first :ast-info))
-                              node))
-                           first
-                           :ast-info
-                           :env
-                           :locals
-                           keys
-                           set))
-          "Failed to add locals to env.")
-    (t/is (= 4 (count (filter (comp #{:local} :op :ast-info first) locs)))
-          "Failed to identify all op-locals")))
+(def let-in-let
+  "(do
+  (println \"foobar\" )
+  (let [a 1
+        b a
+        c 3]
+    (println a)
+    (let [c (inc c)]
+      [b c]))
+  (println \"baz\"))")
 
-;; test let in let with shadowing
+(t/deftest parse-let-loc-test
+  (t/testing "test let embedded into a do"
+      (let [embedded-let-with-locals (trin/parse-loc {} (zip/of-string embedded-let))
+            embedded-let-locs        (trin/all-zlocs embedded-let-with-locals)]
+        (t/is (= '#{a b c} (-> embedded-let-with-locals
+                               zip/down
+                               zip/right
+                               zip/right
+                               zip/down
+                               zip/right
+                               zip/right
+                               first
+                               :ast-info
+                               :env
+                               :locals
+                               keys
+                               set))
+              "Failed to add locals to env.")
+        (t/is (= 4 (count (filter (comp #{:local} :op :ast-info first) embedded-let-locs)))
+              "Failed to identify all op-locals")))
+
+  (t/testing "test let in let, effectively shadowing"
+    (let [let-in-let-locs (->> (zip/of-string let-in-let)
+                               (trin/parse-loc {})
+                               trin/all-zlocs)
+          op-local-locs   (filter (comp #{:local} :op :ast-info first) let-in-let-locs)]
+      (t/is (= 5 (count op-local-locs))
+            "Failed to identify all op-locals")
+      (t/is (= 3 (-> (drop 2 op-local-locs)
+                     ffirst
+                     :ast-info
+                     :init))
+            "Init in outside let for local is incorrect")
+      (let [let-in-let-c-ast (-> (last op-local-locs)
+                                 first
+                                 :ast-info)]
+        (t/is (= '(inc c) (:init let-in-let-c-ast))
+              "Reference to c local is not shadowed in let in let")
+        (t/is (= '{a 1 b a c (inc c)} (:locals (:env let-in-let-c-ast)))
+              "Failed to shadow locals")))))
