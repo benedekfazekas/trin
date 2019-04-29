429aaf651f82b3601ac55d99982c605aa5637f48
Benedek Fazekas <benedek.fazekas@gmail.com>
2019-04-29 14:32:52 +0100

    Implement `find-used-locals` in POC add simple let test

--- find-used-locals-trin-poc/src/thomasa/find_used_locals_trin_poc.clj
+++ find-used-locals-trin-poc/src/thomasa/find_used_locals_trin_poc.clj
@@ -1,6 +1,7 @@
 (ns thomasa.find-used-locals-trin-poc
   (:require [rewrite-clj.zip :as zip]
-            [thomasa.trin :as trin]))
+            [thomasa.trin :as trin]
+            [clojure.set :as set]))
 
 (defn foo
   "I don't do a whole lot."
@@ -8,3 +9,78 @@
   (println x "Hello, World!")
   (trin/parse-loc {} (zip/of-string "[1 2 3]"))
   :foo)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; borrowed from refactor-nrepl for now
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn all-zlocs
+  "Generate a seq of all zlocs in a depth-first manner"
+  [zipper]
+  (take-while (complement zip/end?) (iterate zip/next zipper)))
+
+(defn- node-at-loc?
+  "True if node encloses point defined by `loc-line` and `loc-column`."
+  [zloc ^long loc-line ^long loc-column]
+  (let [[line end-line column end-column] (->> (zip/node zloc)
+                                               meta
+                                               ((juxt :row :end-row :col :end-col))
+                                               (map (comp dec long)))]
+    (or (< line loc-line end-line)
+        (and (or (= line loc-line)
+                 (= end-line loc-line))
+             (<= column loc-column end-column)))))
+
+(defn- zip-to
+  "Move the zipper to the node at `loc-line` and `loc-col`.
+
+  Implementation uses `all-zlocs` and exploits the fact that it generates
+  a seq of nodes in depth-first order."
+  [zipper ^long loc-line ^long loc-column]
+  (reduce
+   (fn [node-at-loc zloc]
+     (if (node-at-loc? zloc loc-line loc-column) zloc node-at-loc))
+   zipper
+   (all-zlocs zipper)))
+
+(defn get-enclosing-sexp
+  "Extracts the sexp enclosing point at LINE and COLUMN in FILE-CONTENT,
+  and optionally LEVEL.
+
+  A string is not treated as a sexp by this function. If LEVEL is
+  provided finds the enclosing sexp up to level. LEVEL defaults to 1
+  for the immediate enclosing sexp.
+
+  Both line and column are indexed from 0."
+  ([parsed-content line column]
+   (get-enclosing-sexp parsed-content line column 1))
+  ([parsed-content ^long line ^long column ^long level]
+   (let [zloc (zip-to parsed-content line column)
+         zloc (nth (iterate zip/up zloc) (dec level))]
+     (cond
+       (and zloc (string? (zip/sexpr zloc))) (zip/up zloc)
+       (and zloc (seq? (zip/sexpr zloc))) zloc
+       zloc (zip/up zloc)
+       :else (throw (ex-info "Can't find sexp boundary"
+                             {:file-content (zip/sexpr parsed-content)
+                              :line line
+                              :column column}))))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; borrowed from refactor-nrepl ends
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+(defn find-used-locals
+  "Adapted version of refactor-nrepl's `refactor-nrepl.find.find-locals/find-used-locals`."
+  [source ^long line ^long column]
+  (let [parsed-source    (trin/parse-loc {} (zip/of-string source))
+        selected-sexp    (get-enclosing-sexp parsed-source line column)
+        locals-in-use    (->> (all-zlocs selected-sexp)
+                              (filter #(= :local (get-in % [0 :ast-info :op])))
+                              (map zip/sexpr)
+                              set)
+        available-locals (-> selected-sexp first :ast-info :env :locals keys set)]
+    (println "selected sexp" (zip/sexpr selected-sexp)
+             " avail locals: " available-locals)
+    (filter  (set/intersection available-locals locals-in-use) available-locals)))
--- find-used-locals-trin-poc/test/thomasa/find_used_locals_trin_poc_test.clj
+++ find-used-locals-trin-poc/test/thomasa/find_used_locals_trin_poc_test.clj
@@ -7,3 +7,19 @@
     (t/is (= 1 1)))
   (t/testing "depending on local trin"
     (t/is (= :foo (sut/foo :ignore)))))
+
+(def no-args-defn-with-let
+  "(defn not-doing-much []
+    (map name [:a :b :c])
+    (let [prefix \"prefix-\"
+          postfix \"-postfix\"]
+      (->> (map name [:a :b :c])
+           (map #(str prefix %)))))")
+
+(t/deftest test-simple-let
+  (t/is (= '() (sut/find-used-locals no-args-defn-with-let 1 10))
+        "Used locals not empty outside let.")
+  (t/is (= '() (sut/find-used-locals no-args-defn-with-let 3 12))
+        "Used locals not empty in bindings form of let")
+  (t/is (= '(prefix) (sut/find-used-locals no-args-defn-with-let 5 21))
+        "Used local should be symbol prefix and only prefix"))
